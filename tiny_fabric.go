fdc2aa3e71fd1ede90d39633b66760b336c50819
Brent Bradbury <brent@brentium.com>
2014-11-30 18:09:06 -0600

    Update with a working docker client, suggest new rest framework

--- tiny_fabric.go
+++ tiny_fabric.go
@@ -2,25 +2,48 @@ package main
 
 import (
 	"fmt"
-	"github.com/zenazn/goji"
-	"github.com/zenazn/goji/web"
-	"net/http"
+	"github.com/fsouza/go-dockerclient"
+	"github.com/gin-gonic/gin"
 )
 
-func hello(c web.C, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome too the home page, %s!", c.URLParams["name"])
+//ServiceManager holds the magic
+type ServiceManager struct {
+	DockerClient *docker.Client
 }
 
-func createService(c web.C, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Creating service %s...", c.URLParams["name"])
+type Service struct {
+	Name string
+}
+
+func NewServiceManager() (manager *ServiceManager, err error) {
+	manager = new(ServiceManager)
+	manager.DockerClient, err = docker.NewClient("tcp://192.168.59.103:2375")
+	if err != nil {
+		err = fmt.Errorf("service_manager couldn't start docker.Client: %s", err)
+	}
+	return
+}
+
+func (s *ServiceManager) NewServiceInstance(c *gin.Context) {
+	c.String(200, "Started a service!")
+}
+
+func hello(c *gin.Context) {
+	c.String(200, "Welcome too the home page, %s!", c.Params.ByName("name"))
 }
 
 func main() {
-	goji.Get("/hello/:name", hello)
-	goji.Get("/services", func(_ http.ResponseWriter, _ *http.Request) {})
-	goji.Post("/services/:service_name", createService)
-	goji.Post("/services/:service_name/instances/:instance_id", func(_ http.ResponseWriter, _ *http.Request) {})
-	goji.Post("/services/:service_name/instances/:instance_id/logs", func(_ http.ResponseWriter, _ *http.Request) {})
-	goji.Post("/services/:service_name/instances/:instance_id/logs/:log_id", func(_ http.ResponseWriter, _ *http.Request) {})
-	goji.Serve()
+	manager, _ := NewServiceManager()
+	fmt.Print(manager)
+	//todo: probably use https://github.com/emicklei/go-restful
+	router := gin.Default()
+	router.GET("/hello/:name", hello)
+	router.POST("/services/:service_name/instances", manager.NewServiceInstance)
+	/*router.GET("/services", func(_ http.ResponseWriter, _ *http.Request) {})
+	router.POST("/services/:service_name", createService)
+	router.POST("/services/:service_name/instances", func(_ http.ResponseWriter, _ *http.Request) {})
+	router.GET("/services/:service_name/instances/:instance_id", func(_ http.ResponseWriter, _ *http.Request) {})
+	router.POST("/services/:service_name/instances/:instance_id/logs", func(_ http.ResponseWriter, _ *http.Request) {})
+	router.POST("/services/:service_name/instances/:instance_id/logs/:log_id", func(_ http.ResponseWriter, _ *http.Request) {}) */
+	router.Run(":8080")
 }
